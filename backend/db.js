ca5ca2ea8fadb28270f4680083445b42aadb275a
italoweb99 <italoweb99@gmail.com>
2025-09-10 20:55:20 -0300

    migração do banco para o supabase e atualizações necessarias para que o banco funcione

--- backend/db.js
+++ backend/db.js
@@ -1,8 +1,22 @@
 const {Pool} = require('pg')
-
-
+require('dotenv').config()
+const {Client} = require('pg')
+const pool = new Client({
+  connectionString: process.env.CONNECTIONSTRING
+  
+})
+async function  connetSupa(){
+try{
+  pool.connect()
+}
+catch(err){
+  console.log(err)
+}
+}
+connetSupa()
+module.exports = pool
 // Configurar conexão com o banco de dados PostgreSQL
-const pool = new Pool({
+/*const pool = new Pool({
   user: 'postgres',
   host: 'localhost',
   database: 'kplus',
@@ -10,4 +24,4 @@ const pool = new Pool({
   port: 5432,
 });
 
-module.exports = pool;
+module.exports = pool;*/
